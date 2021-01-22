56a40b06e85fedf7749bb44d53964a88891e4fd4
PabloRSantos <pablorsantos15@gmail.com>
2021-01-22 17:38:27 -0300

    feat: dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages, auth
 from django.core.validators import validate_email
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from .models import FormContact
 
 
 def login(request):
@@ -76,4 +77,18 @@ def register(request):
 
 @login_required(redirect_field_name='login')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    if request.method != 'POST':
+        form = FormContact()
+        return render(request, 'accounts/dashboard.html', {'form': form})
+
+    form = FormContact(request.POST, request.FILES)
+
+    if not form.is_valid():
+        messages.error(request, 'Erro ao enviar formulário')
+        form = FormContact(request.POST)
+        return render(request, 'accounts/dashboard.html', {'form': form})
+
+    form.save()
+    messages.success(
+        request, f'Contato {request.POST.get("name")} salvo com sucesso')
+    return redirect('dashboard')
