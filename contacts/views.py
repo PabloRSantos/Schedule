7a452c8522584b7cb5fa965fc1f1399b4b460e4c
PabloRSantos <pablorsantos15@gmail.com>
2021-01-22 11:11:13 -0300

    feat: search contact

--- contacts/views.py
+++ contacts/views.py
@@ -2,6 +2,8 @@ from django.http.response import Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Contact
 from django.core.paginator import Paginator
+from django.db.models import Q, Value
+from django.db.models.functions import Concat
 
 
 def index(request):
@@ -26,4 +28,28 @@ def contact_profile(request, contact_id):
 
     return render(request, 'contacts/profile.html', {
         'contact': contact
+    })
+
+
+def search(request):
+    term = request.GET.get('term')
+
+    if term is None:
+        raise Http404()
+
+    fields = Concat('name', Value(' '), 'surname')
+
+    contacts = Contact.objects.annotate(
+        complete_name=fields,
+    ).filter(
+        Q(complete_name__icontains=term) |
+        Q(telephone__icontains=term),
+        show=True
+    )
+    paginator = Paginator(contacts, 10)
+    page = request.GET.get('page')
+    contacts = paginator.get_page(page)
+
+    return render(request, 'contacts/search.html', {
+        'contacts': contacts
     })
