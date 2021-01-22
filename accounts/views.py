297b8cf3c2751c887c2d5c67545862144a99dbcf
PabloRSantos <pablorsantos15@gmail.com>
2021-01-22 17:22:15 -0300

    feat: authentication

---
+++ accounts/views.py
@@ -0,0 +1,79 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.core.validators import validate_email
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+
+def login(request):
+    if request.method != 'POST':
+        return render(request, 'accounts/login.html')
+
+    username = request.POST.get('user')
+    password = request.POST.get('password')
+
+    user = auth.authenticate(request, username=username, password=password)
+    if not user:
+        messages.error(request, 'Usuário ou senha inválido')
+        return render(request, 'accounts/login.html')
+
+    auth.login(request, user)
+    messages.success(request, 'Logado com sucesso')
+    return redirect('dashboard')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('index')
+
+
+def register(request):
+    if request.method != 'POST':
+        return render(request, 'accounts/register.html')
+
+    name = request.POST.get('name')
+    surname = request.POST.get('surname')
+    email = request.POST.get('email')
+    user = request.POST.get('user')
+    password = request.POST.get('password')
+    password2 = request.POST.get('password2')
+
+    try:
+        validate_email(email)
+    except:
+        messages.error(request, 'Email inválido')
+        return render(request, 'accounts/register.html')
+
+    if len(password) < 6:
+        messages.error(request, 'Senha precisa ter 6 caracteres ou mais')
+        return render(request, 'accounts/register.html')
+
+    if len(user) < 6:
+        messages.error(request, 'Usuário precisa ter 6 caracteres ou mais')
+        return render(request, 'accounts/register.html')
+
+    if password != password2:
+        messages.error(request, 'Senhas não conferem')
+        return render(request, 'accounts/register.html')
+
+    if User.objects.filter(username=user).exists():
+        messages.error(request, 'Usuário já existe')
+        return render(request, 'accounts/register.html')
+
+    if User.objects.filter(username=email).exists():
+        messages.error(request, 'Email já existe')
+        return render(request, 'accounts/register.html')
+
+    messages.success(request, 'Registrado com sucesso!')
+
+    user = User.objects.create(
+        email=email, username=user, password=password,
+        first_name=name, last_name=surname)
+
+    user.save()
+    return redirect('login')
+
+
+@login_required(redirect_field_name='login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
